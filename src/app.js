3958b63dce39691f12988ac2fbabb27996859df6
muskankapoor <muskankapoor016@gmail.com>
2021-04-03 17:36:19 -0400

    update moudle 2

--- src/app.js
+++ src/app.js
@@ -1,18 +1,49 @@
+// require built-in libraries 
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
-
 const app = express();
 
+// configure the view and static directory 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
+// readFileSync to read contetns of file
+const accountData = fs.readFileSync('src/json/accounts.json', encoding='utf-8');
+const accounts = JSON.parse(accountData);
 
+const userData = fs.readFileSync('src/json/users.json', encoding='utf-8');
+const users = JSON.parse(userData);
+
+// index route 
 app.get('/', (req, res) => {
-    res.render('index', { title: 'Index'})
+    res.render('index', { title: 'Account Summary'}, {accounts: accounts});
 });
 
+// savings route
+app.get('/savings', (req, res) => {
+    res.render('account', { account: accounts.savings });
+});
+
+// checking route
+app.get('/checking', (req, res) => {
+    res.render('account', { account: accounts.checking });
+});
+
+// credit route
+app.get('/credit', (req, res) => {
+    res.render('account', { account: accounts.credit });
+});
+
+// profile route
+app.get('/profile', (req, res) => {
+    res.render('profile', { user: users[0] });
+});
+
+
+// create a server
 app.listen(3000, function(){
     console.log("PS Project Running on port 3000!");
 });
+
